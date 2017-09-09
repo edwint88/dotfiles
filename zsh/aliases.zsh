c9376d3ec8fc51428f926e7c9589c4db1664527b
edwint88 <tuzaredwin@gmail.com>
2017-09-10 01:55:59 +0200

    added symlinks

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,27 +1,10 @@
-# Lines configured by zsh-newuser-install
-HISTFILE=~/.histfile
-HISTSIZE=10000
-SAVEHIST=10000
-bindkey -e
-# End of lines configured by zsh-newuser-install
-# The following lines were added by compinstall
-zstyle :compinstall filename '/Users/zartu/.zshrc'
-
-autoload -Uz compinit
-compinit
-# End of lines added by compinstall
-
-# Completion
-# matches case insensitive for lowercase
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-
 # Aliases
 
 # Detect which `ls` flavor is in use
 if ls --color > /dev/null 2>&1; then # GNU `ls`
-   colorflag="--color"
+    colorflag="--color"
 else # OS X `ls`
-   colorflag="-G"
+       colorflag="-G"
 fi
 
 # reload zsh config
@@ -29,7 +12,7 @@ alias reload!='RELOAD=1 source ~/.zshrc'
 
 # Custom cd
 cdls() {
-	cd $1;
+  	cd $1;
 	ls;
 }	
 
@@ -51,4 +34,3 @@ alias ta="tmux attach -d -t"
 alias tm="tmux ls"
 alias tms="tmux new -s"
 alias tks="tmux kill-session -t"
-
