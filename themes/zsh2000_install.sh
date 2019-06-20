aa03edd6c22b8e4ce931d7ee0a6c695f39c30812
Edwin Tuzar <edwin.tuzar@contractors.twinformatics.at>
2019-06-20 16:11:40 +0200

    fix github

--- themes/zsh2000_install.sh
+++ themes/zsh2000_install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if [ -z $HOME_DOTFILES ]; then
+if [ -z "$HOME_DOTFILES" ]; then
 	echo "set HOME_DOTFILES first";
 	exit 1;
 else
 	echo "Path HOME_DOTFILES:$HOME_DOTFILES";
-fi;
+fi
 
 # instal rvm if is not installed
-if [[ `which rvm` == *"not found" ]]; then
+if [[ $(command which rvm) == *"not found" ]]; then
 	\curl -L https://get.rvm.io | bash -s stable --rails --autolibs=enable;
 else
 	echo "rvm already installed";
@@ -16,22 +16,22 @@ else
 fi
 
 # check if the zsh2000 is empty
-if [ -z "$(ls -A $HOME_DOTFILES/themes/zsh2000)" ]; then
-	rmdir $HOME_DOTFILES/theme/zsh2000;
+if [ -z "$(ls -A "$HOME_DOTFILES/themes/zsh2000")" ]; then
+	rmdir "$HOME_DOTFILES/theme/zsh2000";
 else
 	echo "zsh2000 module files are available";
 fi
 
 # clone theme if is not clonned
-if [ ! -d $HOME_DOTFILES/themes/zsh2000 ]; then
-	git clone https://github.com/maverick9000/zsh2000.git $HOME_DOTFILES/themes/zsh2000;
+if [ ! -d "$HOME_DOTFILES/themes/zsh2000" ]; then
+	git clone https://github.com/maverick9000/zsh2000.git "$HOME_DOTFILES/themes/zsh2000";
 else
 	echo "theme already clonned";
 fi
 
 # link theme
-if [ ! -e $HOME_DOTFILES/oh-my-zsh/themes/zsh2000.zsh-theme ]; then
-	ln -s $HOME_DOTFILES/themes/zsh2000/zsh2000.zsh-theme $HOME_DOTFILES/oh-my-zsh/themes/zsh2000.zsh-theme;
+if [ ! -e "$HOME_DOTFILES/oh-my-zsh/themes/zsh2000.zsh-theme" ]; then
+	ln -s "$HOME_DOTFILES/themes/zsh2000/zsh2000.zsh-theme" "$HOME_DOTFILES/oh-my-zsh/themes/zsh2000.zsh-theme";
 else
 	echo "theme already linked";
 fi
